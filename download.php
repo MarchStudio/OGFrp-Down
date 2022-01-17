7b7609d5c179db5c3a802bbded8a2aa65db36e9e
黄宇孟 <hym2007@outlook.com>
2022-01-17 08:45:14 +0800

    220117.02

--- download.php
+++ download.php
@@ -25,7 +25,7 @@ if(!$rs) {
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1 class="m-0 text-dark"><?php echo $page_title; ?>&nbsp;&nbsp;<small class="text-muted text-xs">下载各种版本的 Frp 客户端</small></h1></div>
+                <h1 class="m-0 text-dark"><?php echo $page_title; ?>&nbsp;&nbsp;<small class="text-muted text-xs">下载适用于各种平台的OGFrp客户端</small></h1></div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
                     <li class="breadcrumb-item">
@@ -43,6 +43,7 @@ if(!$rs) {
                     <div class="card-header border-0">
                         <div class="d-flex justify-content-between">
                             <h3 class="card-title">客户端下载   <a href="https://client.ogfrp.cn">https://client.ogfrp.cn<a></h3>
+                            <h3 class="card-title">GitHub下载源   <a href="https://github.com/OldGodShen/OGFrp/releases">https://github.com/OldGodShen/OGFrp/releases<a></h3>
                         </div>
                     </div>
                     <div class="card-body p-0 table-responsive">
@@ -63,7 +64,7 @@ if(!$rs) {
 								<tr>
 									<td><img src="assets/download/windows.png" class="system-img"></td>
 									<td nowrap>Windows</td>
-									<td nowrap>64位</td>
+									<td nowrap>x86，兼容AMD64及ARM</td>
 									<td nowrap>命令行版</td>
 									<td nowrap><!--<a href="" target="_blank"><button class="btn btn-sm btn-success">-->null</button></a></td>
 									<td nowrap><a href="https://client.ogfrp.cn/OGFrp/OGFrp.Lite.zip" target="_blank"><button class="btn btn-sm btn-success">点击下载</button></a></td>
@@ -74,8 +75,8 @@ if(!$rs) {
 								<tr>
 									<td><img src="assets/download/windows.png" class="system-img"></td>
 									<td nowrap>Windows</td>
-									<td nowrap>64位</td>
-									<td nowrap>GUI版</td>
+									<td nowrap>64</td>
+									<td nowrap>图形界面版</td>
 									<td nowrap><!--<a href="" target="_blank"><button class="btn btn-sm btn-success">-->null</button></a></td>
 									<td nowrap><a href="https://client.ogfrp.cn/OGFrp/OGFrp.GUI.zip" target="_blank"><button class="btn btn-sm btn-success">点击下载</button></a></td>
 									<td nowrap><code>https://client.ogfrp.cn/OGFrp/OGFrp.GUI.zip</code></td>
@@ -85,7 +86,7 @@ if(!$rs) {
 								<tr>
 									<td><img src="assets/download/linux.png" class="system-img"></td>
 									<td nowrap>Linux</td>
-									<td nowrap>x86/amd64/ARM/ARM64</td>
+									<td nowrap>x86/AMD64/ARM/ARM64</td>
 									<td nowrap>命令行版</td>
 									<td nowrap><!--<a href="" target="_blank"><button class="btn btn-sm btn-success">-->null</button></a></td>
 									<td nowrap><a href="https://client.ogfrp.cn/OGFrp/OGFrp.Linux.zip" target="_blank"><button class="btn btn-sm btn-success">点击下载</button></a></td>
